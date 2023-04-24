ca757ac147287e525da1b7b393004b25194b89db
Victor Omoniyi <vomoniyi2001@gmail.com>
2023-04-24 01:24:06 +0100

    Auth Controller

--- Backend/src/controllers/auth.controller.ts
+++ Backend/src/controllers/auth.controller.ts
@@ -18,3 +18,24 @@ const register = async (req: Request, res: Response) => {
         res.status(errors.status).json({message: errors.message, success: false});
     }
 }   
+
+const login = async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+    try {
+        const user = await User.findOne({email});
+        if(user){
+            const isMatch = await bcrypt.compare(password, user.password);
+            if(isMatch){
+                const token = jwt.sign({id: user._id}, process.env.JWT_SECRET, {expiresIn: '1d'});
+                res.status(200).json({message: "Login successful", success: true, token});
+            }else{
+                res.status(400).json({message: "Password incorrect", success: false});
+            }
+        }else{
+            res.status(404).json({message: "User not found", success: false});
+        }
+    } catch (error) {
+        const errors = errorHandler(error);
+        res.status(errors.status).json({message: errors.message, success: false});
+    }
+}
